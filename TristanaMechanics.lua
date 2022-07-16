27a07b82108185ca7fcb6097fffd04f495d8ae52
hagbardlol <59706487+hagbardlol@users.noreply.github.com>
2022-07-16 10:25:27 -0800

    Removed TristanaMechanics 1.0.8

--- TristanaMechanics.lua
+++ TristanaMechanics.lua
@@ -1,91 +1,85 @@
-if _G.CoreEx.ObjectManager.Player.CharName ~= "Tristana" then
-    return
-end
-local ScriptName, Version = "TristanaMechanics", "1.0.7"
-_G.CoreEx.AutoUpdate("https://raw.githubusercontent.com/hagbardlol/nosupport/main/" .. ScriptName ..".lua", Version)
+if Player.CharName ~= "Tristana" then return end
 
-local SDK = _G.CoreEx
-local Lib = _G.Libs
-local Obj = SDK.ObjectManager
-local Player = Obj.Player
-local Event = SDK.EventManager
-local Enums = SDK.Enums
-local Renderer = SDK.Renderer
-local Input = SDK.Input
-local HealthPred = Lib.HealthPred
+module("TristanaMechanics", package.seeall, log.setup)
+clean.module("TristanaMechanics", clean.seeall, log.setup)
+CoreEx.AutoUpdate("https://raw.githubusercontent.com/hagbardlol/Public/main/TristanaMechanics.lua", "1.0.8")
+local CoreEx = _G.CoreEx
+local Enums, EventManager, Renderer, Game, Vector = CoreEx.Enums, CoreEx.EventManager, CoreEx.Renderer, CoreEx.Game, CoreEx.Geometry.Vector
+local Screen = Renderer.GetResolution()
 
-local TS = Lib.TargetSelector()
-local HitChanceEnum = SDK.Enums.HitChance
-local Menu = Lib.NewMenu
-local Orb = Lib.Orbwalker
-local Pred = Lib.Prediction
-local DmgLib = Lib.DamageLib
-local Spell = Lib.Spell
-local Geometry = SDK.Geometry
-local Path = Geometry.Path
-local Vector = Geometry.Vector
+local Text = {
+    Title = "Unfortunately, this assembly is no longer supported by us.",
+    Subtitle = "No need to panic! We have made a new assembly available for you. Just check the forum or download the new assembly with the in-game downloader.",
+    Message = "Please disable this assembly in your loader and press F5 to make the message disappear!",
+}
 
-local basePosition = Player.TeamId == 100 and Vector(14302, 172, 14387) or Vector(415, 182, 415)
+local Size = {
+    Border = Vector(800, 230), -- width, height
+    Background = Vector(799, 228),
+    Logo = Vector(350, 100),
+    Warning = Vector(40, 40),
+    Text = {
+        Title = Renderer.CalcTextSize(Text.Title),
+        Subtitle = Renderer.CalcTextSize(Text.Subtitle),
+        Message = Renderer.CalcTextSize(Text.Message),
+    }
+}
 
+local Position = {
+    Border = Vector((Screen.x / 2) - Size.Border.x / 2, (Screen.y / 2) - Size.Border.y / 2),
+    Background = Vector((Screen.x / 2) - Size.Background.x / 2, (Screen.y / 2) - Size.Background.y / 2),
+    Logo = Vector(Screen.x / 2, Screen.y / 2 - Size.Logo.y / 2),
+    Line = {
+        Start = Vector((Screen.x / 2 - Size.Border.x / 2) + 10, (Screen.y / 2 - Size.Logo.y / 2) + 60),
+        End =  Vector((Screen.x / 2 + Size.Border.x / 2) - 10, (Screen.y / 2 - Size.Logo.y / 2) + 60)
+    },
+    Text = {
+        Title = Vector((Screen.x / 2) - Size.Text.Title.x / 2, (Screen.y / 2 - Size.Logo.y / 2) + 60 + Size.Text.Title.y / 2),
+        Subtitle = Vector((Screen.x / 2) - Size.Text.Subtitle.x / 2, (Screen.y / 2 - Size.Logo.y / 2) + 60 + Size.Text.Title.y + Size.Text.Subtitle.y / 2),
+        Message = Vector((Screen.x / 2) - Size.Text.Message.x / 2, (Screen.y / 2 - Size.Logo.y / 2) + 60 + Size.Text.Title.y + Size.Text.Subtitle.y + Size.Text.Message.y / 2),
+    },
+    Warning = Vector((Screen.x / 2) - Size.Warning.x / 2, (Screen.y / 2 - Size.Logo.y / 2) + 60 + Size.Text.Title.y + Size.Text.Subtitle.y + Size.Text.Message.y + Size.Warning.y / 2 + 10)
+}
 
-TristanaMechanics = BaseStrucutre:new()
+local Logo = Renderer.CreateSprite("robur.png", Size.Logo.x, Size.Logo.y)
+local WarningSprite = Renderer.CreateSprite("warning.png", Size.Warning.x, Size.Warning.y)
 
-TristanaMechanics:Menu()
-
--- Loading Components --
-
--- Events --
-local OnNormalPriority = function()
-    TristanaMechanics:OnLoop()
-end
-
-local OnGapClose = function(source, dash)
-    TristanaMechanics:OnGapClose(source, dash)
-end
-
-local OnInterruptibleSpell = function(Source, SpellCast, Danger, EndTime, CanMoveDuringChannel)
-    TristanaMechanics:OnInterruptibleSpell(Source, SpellCast, Danger, EndTime, CanMoveDuringChannel)
-end
-
-local OnLowPriority = function()
-    TristanaMechanics:OnLowPriority()
+local function DrawBackground()
+    Renderer.DrawRectOutline(Position.Border, Size.Border, 1, 2, 0x001A332F)
+    Renderer.DrawFilledRect(Position.Background, Size.Background, 1, 0x0D1B18FF)
 end
 
-local OnPostAttack = function()
-    TristanaMechanics:OnPostAttack()
+local function DrawLogo()
+    Logo:Draw(Position.Logo, 0, true)
 end
 
-local OnDraw = function()
-    TristanaMechanics:OnDraw()
+local function DrawWarning()
+    if math.floor(Game.GetTime() * 10) % 2 == 0 then
+        WarningSprite:Draw(Position.Warning, 0, true)
+    end
 end
 
-local OnVisionLost = function(obj)
-    TristanaMechanics:OnVisionLost(obj)
+local function DrawLine()
+    Renderer.DrawLine(Position.Line.Start, Position.Line.End, 1, 0xBFA64BFF)
 end
 
-local OnCreateObject = function(obj)
-    TristanaMechanics:OnCreateObject(obj)
+local function DrawText()
+    Renderer.DrawText(Position.Text.Title, Size.Text.Title, Text.Title, 0xFF0000FF)
+    Renderer.DrawText(Position.Text.Subtitle, Size.Text.Subtitle, Text.Subtitle, 0x00FF00FF)
+    Renderer.DrawText(Position.Text.Message, Size.Text.Message, Text.Message, 0xFFD966FF)
 end
 
-local OnPreAttack = function(args)
-    TristanaMechanics:OnPreAttack(args)
+
+local function OnDraw()
+    DrawBackground()
+    DrawLogo()
+    DrawLine()
+    DrawText()
+    DrawWarning()
 end
 
-local OnDrawDamage = function(target, dmgList)
-    TristanaMechanics:OnDrawDamage(target, dmgList)
-end
 
-function OnLoad()
-    Event.RegisterCallback(Enums.Events.OnLowPriority, OnLowPriority)
-    Event.RegisterCallback(Enums.Events.OnTick, OnNormalPriority)
-    Event.RegisterCallback(Enums.Events.OnGapclose, OnGapClose)
-    Event.RegisterCallback(Enums.Events.OnInterruptibleSpell, OnInterruptibleSpell)
-    Event.RegisterCallback(Enums.Events.OnPostAttack, OnPostAttack)
-    Event.RegisterCallback(Enums.Events.OnVisionLost, OnVisionLost)
-    Event.RegisterCallback(Enums.Events.OnPreAttack, OnPreAttack)
-    Event.RegisterCallback(Enums.Events.OnDraw, OnDraw)
-    Event.RegisterCallback(Enums.Events.OnDrawDamage, OnDrawDamage)
-    CheckIgniteSlot()
-
-    return true
+local function Initialize()
+    EventManager.RegisterCallback(Enums.Events.OnDraw, OnDraw)
 end
+Initialize()
