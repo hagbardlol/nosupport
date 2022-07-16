6a80c4001f3ff92fb69aa70382a35847351fa932
hagbardlol <59706487+hagbardlol@users.noreply.github.com>
2022-07-16 10:23:10 -0800

    Removed PewPewTristana 1.0.7

--- PewPewTristana.lua
+++ PewPewTristana.lua
@@ -1,63 +1,85 @@
-if _G.CoreEx.ObjectManager.Player.CharName ~= "Tristana" then
-    return
-end
+if Player.CharName ~= "Tristana" then return end
 
 module("PewPewTristana", package.seeall, log.setup)
 clean.module("PewPewTristana", clean.seeall, log.setup)
+CoreEx.AutoUpdate("https://raw.githubusercontent.com/hagbardlol/Public/main/PewPewTristana.lua", "1.0.7")
+local CoreEx = _G.CoreEx
+local Enums, EventManager, Renderer, Game, Vector = CoreEx.Enums, CoreEx.EventManager, CoreEx.Renderer, CoreEx.Game, CoreEx.Geometry.Vector
+local Screen = Renderer.GetResolution()
 
-_G.CoreEx.AutoUpdate("https://raw.githubusercontent.com/hagbardlol/nosupport/main/PewPewTristana.lua", "1.0.6")
-
-local API = {
-    Game = _G.CoreEx.Game,
-    Input = _G.CoreEx.Input,
-    Vector = _G.CoreEx.Geometry.Vector,
-    Polygon = _G.CoreEx.Geometry.Polygon,
-    Player = _G.Player,
-    Circle = _G.CoreEx.Geometry.Circle,
-    Cone = _G.CoreEx.Geometry.Cone,
-    EventManager = _G.CoreEx.EventManager,
-    Enums = _G.CoreEx.Enums,
-    Libs = _G.Libs,
-    GetCurrentMillis = _G.getCurrentMillis,
-    Renderer = _G.CoreEx.Renderer,
-    ObjectManager = _G.CoreEx.ObjectManager,
-    Nav = _G.CoreEx.Nav,
-    SpellLib = _G.Libs.Spell,
-    Orbwalker = _G.Libs.Orbwalker,
-    TargetSelector = _G.Libs.TargetSelector,
-    CollisionLib = _G.Libs.CollisionLib,
-    HealthPred = _G.Libs.HealthPred,
-    Evade = _G.CoreEx.EvadeAPI
+local Text = {
+    Title = "Unfortunately, this assembly is no longer supported by us.",
+    Subtitle = "No need to panic! We have made a new assembly available for you. Just check the forum or download the new assembly with the in-game downloader.",
+    Message = "Please disable this assembly in your loader and press F5 to make the message disappear!",
 }
 
-local Menu = API.Libs.NewMenu
-local TS = API.TargetSelector()
-local Tristana = {
-    Q = API.SpellLib.Active({
-        Slot = API.Enums.SpellSlots.Q,
-        Key = "Q"
-    }),
-    W = API.SpellLib.Skillshot({
-        Slot = API.Enums.SpellSlots.W,
-        Key = "W",
-        Range = 900
-    }),
-    E = API.SpellLib.Targeted({
-        Slot = API.Enums.SpellSlots.E,
-        Key = "E"
-    }),
-    R = API.SpellLib.Targeted({
-        Slot = API.Enums.SpellSlots.R,
-        Key = "R"
-    })
+local Size = {
+    Border = Vector(800, 230), -- width, height
+    Background = Vector(799, 228),
+    Logo = Vector(350, 100),
+    Warning = Vector(40, 40),
+    Text = {
+        Title = Renderer.CalcTextSize(Text.Title),
+        Subtitle = Renderer.CalcTextSize(Text.Subtitle),
+        Message = Renderer.CalcTextSize(Text.Message),
+    }
 }
 
+local Position = {
+    Border = Vector((Screen.x / 2) - Size.Border.x / 2, (Screen.y / 2) - Size.Border.y / 2),
+    Background = Vector((Screen.x / 2) - Size.Background.x / 2, (Screen.y / 2) - Size.Background.y / 2),
+    Logo = Vector(Screen.x / 2, Screen.y / 2 - Size.Logo.y / 2),
+    Line = {
+        Start = Vector((Screen.x / 2 - Size.Border.x / 2) + 10, (Screen.y / 2 - Size.Logo.y / 2) + 60),
+        End =  Vector((Screen.x / 2 + Size.Border.x / 2) - 10, (Screen.y / 2 - Size.Logo.y / 2) + 60)
+    },
+    Text = {
+        Title = Vector((Screen.x / 2) - Size.Text.Title.x / 2, (Screen.y / 2 - Size.Logo.y / 2) + 60 + Size.Text.Title.y / 2),
+        Subtitle = Vector((Screen.x / 2) - Size.Text.Subtitle.x / 2, (Screen.y / 2 - Size.Logo.y / 2) + 60 + Size.Text.Title.y + Size.Text.Subtitle.y / 2),
+        Message = Vector((Screen.x / 2) - Size.Text.Message.x / 2, (Screen.y / 2 - Size.Logo.y / 2) + 60 + Size.Text.Title.y + Size.Text.Subtitle.y + Size.Text.Message.y / 2),
+    },
+    Warning = Vector((Screen.x / 2) - Size.Warning.x / 2, (Screen.y / 2 - Size.Logo.y / 2) + 60 + Size.Text.Title.y + Size.Text.Subtitle.y + Size.Text.Message.y + Size.Warning.y / 2 + 10)
+}
 
-    for eventName, eventId in pairs(API.Enums.Events) do
-        if Tristana[eventName] then
-            API.EventManager.RegisterCallback(eventId, Tristana[eventName])
-        end
-    end
+local Logo = Renderer.CreateSprite("robur.png", Size.Logo.x, Size.Logo.y)
+local WarningSprite = Renderer.CreateSprite("warning.png", Size.Warning.x, Size.Warning.y)
 
-    return true
+local function DrawBackground()
+    Renderer.DrawRectOutline(Position.Border, Size.Border, 1, 2, 0x001A332F)
+    Renderer.DrawFilledRect(Position.Background, Size.Background, 1, 0x0D1B18FF)
 end
+
+local function DrawLogo()
+    Logo:Draw(Position.Logo, 0, true)
+end
+
+local function DrawWarning()
+    if math.floor(Game.GetTime() * 10) % 2 == 0 then
+        WarningSprite:Draw(Position.Warning, 0, true)
+    end
+end
+
+local function DrawLine()
+    Renderer.DrawLine(Position.Line.Start, Position.Line.End, 1, 0xBFA64BFF)
+end
+
+local function DrawText()
+    Renderer.DrawText(Position.Text.Title, Size.Text.Title, Text.Title, 0xFF0000FF)
+    Renderer.DrawText(Position.Text.Subtitle, Size.Text.Subtitle, Text.Subtitle, 0x00FF00FF)
+    Renderer.DrawText(Position.Text.Message, Size.Text.Message, Text.Message, 0xFFD966FF)
+end
+
+
+local function OnDraw()
+    DrawBackground()
+    DrawLogo()
+    DrawLine()
+    DrawText()
+    DrawWarning()
+end
+
+
+local function Initialize()
+    EventManager.RegisterCallback(Enums.Events.OnDraw, OnDraw)
+end
+Initialize()
